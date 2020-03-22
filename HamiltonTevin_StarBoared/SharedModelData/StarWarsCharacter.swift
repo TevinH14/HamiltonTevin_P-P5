720651a17386d33fea55bb4d097e1fe7f7d36190
Tevin Hamilton <tahamilton@student.fullsail.edu>
2020-03-22 19:51:06 -0400

    START
    START: of project (project had to be created due to issues )

---
+++ HamiltonTevin_StarBoared/SharedModelData/StarWarsCharacter.swift
@@ -0,0 +1,12 @@
+//
+//  StarWarsCharacter.swift
+//  HamiltonTevin_StarBoared
+//
+//  Created by Tevin Hamilton on 3/22/20.
+//  Copyright © 2020 Tevin Hamilton. All rights reserved.
+//
+
+import Foundation
+let name: String
+let gender: String
+let birth_year: String
