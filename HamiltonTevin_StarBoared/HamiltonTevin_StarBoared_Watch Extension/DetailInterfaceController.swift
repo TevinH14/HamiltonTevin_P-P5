d2878397fbc9709b8e05a23c1f8a7363316abac4
Tevin Hamilton <tahamilton@student.fullsail.edu>
2020-03-24 22:20:04 -0400

    Added
    Added a new Detail interface controller to display charater info.

---
+++ HamiltonTevin_StarBoared/HamiltonTevin_StarBoared_Watch Extension/DetailInterfaceController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailInterfaceController.swift
+//  HamiltonTevin_StarBoared_Watch Extension
+//
+//  Created by Tevin Hamilton on 3/24/20.
+//  Copyright © 2020 Tevin Hamilton. All rights reserved.
+//
+
+import Foundation
