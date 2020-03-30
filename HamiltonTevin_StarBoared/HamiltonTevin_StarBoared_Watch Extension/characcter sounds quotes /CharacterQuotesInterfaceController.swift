9e6868d77f9e678cf5d168fc284ef49fdedc7b85
Tevin Hamilton <tahamilton@student.fullsail.edu>
2020-03-30 03:50:29 -0400

    Added
    Added an new interfacecontroller to handle button selection and play sound for the user

---
+++ HamiltonTevin_StarBoared/HamiltonTevin_StarBoared_Watch Extension/characcter sounds quotes /CharacterQuotesInterfaceController.swift
@@ -0,0 +1,28 @@
+//
+//  CharacterquotesInterfaceController.swift
+//  HamiltonTevin_StarBoared_Watch Extension
+//
+//  Created by Tevin Hamilton on 3/30/20.
+//  Copyright © 2020 Tevin Hamilton. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+class CharacterQuotesInterfaceController:InterfaceController {
+
+    override func awake(withContext context: Any?) {
+        
+    }
+    override func willActivate() {
+        
+    }
+    override func didDeactivate() {
+        
+    }
+    @IBAction func playDarthVader() {
+    }
+    @IBAction func playLukeSkywalker() {
+    }
+    @IBAction func playYoda() {
+    }
+}
